f4806441c7027aa37dd5fa08635945c66a5cf10b
Yu Yang <yu.yang2@hpe.com>
2015-11-16 18:16:38 +0800

    Remember password

---
+++ app/src/main/java/com/example/yyang2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yyang2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.yyang2.sharedpreferencestest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
